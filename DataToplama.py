a7f8c9ff16e05ddcb6cd8e3fbcc70d44210e36fc
Mehmet Çağrı ÇALIŞKAN <mcagricaliskan@gmail.com>
2019-11-04 18:30:21 +0300

    Update

---
+++ DataToplama.py
@@ -0,0 +1,38 @@
+import keyboard
+import time
+from grabscreen import grab_screen
+import cv2
+
+
+
+#
+# ####### EKRAN TESTİ İÇİN
+# last_time = time.time()
+# while True:
+#     resim = grab_screen((200,230,1000,640))
+#     resim = cv2.cvtColor(resim, cv2.COLOR_BGR2RGB)
+#     print("Nö")
+#     print("FPS {} ".format(time.time()-last_time))
+#     last_time = time.time()
+#     cv2.imshow("Pencere", resim)
+#     if cv2.waitKey(100) and 0xFF == ord("q"):
+#         cv2.destroyAllWindows()
+#         break
+
+sayı = 0
+while True:
+    key = keyboard.read_key()
+    if key == "k" or key == "K":
+        sayı += 1
+        print("Aynen kanka oldu")
+        resim = grab_screen()
+        resim = cv2.cvtColor(resim, cv2.COLOR_BGR2RGB)
+        cv2.imwrite("Araba\Araba-"+ str(sayı)+".jpg", resim)
+        time.sleep(0.1)
+    if key == "p":
+        while True:
+            cv2.imshow("Pencere", resim)
+            key = cv2.waitKey(0)
+            if key == ord("s"):
+                cv2.destroyAllWindows()
+                break
