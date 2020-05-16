9f87694cda07b101c5e752fa9d494d29ded28204
Mehmet Çağrı ÇALIŞKAN <mcagricaliskan@gmail.com>
2020-05-16 13:43:32 +0300

    Update

--- TestModel.py
+++ TestModel.py
@@ -5,7 +5,7 @@ import cv2
 import numpy as np
 from keras.models import load_model
 
-model = load_model("GTA(25.12.5).h5")
+model = load_model("GTA(16.05.20)-5.h5")
 
 ####### TEST ALEXNET MODEL #########
 
@@ -14,15 +14,14 @@ while True:
     Image = grab_screen((101, 51, 650, 600))
     Image = cv2.cvtColor(Image, cv2.COLOR_BGR2RGB)
     prediction = cv2.cvtColor(Image, cv2.COLOR_RGB2GRAY)
-    prediction = cv2.resize(prediction, (224, 224))
+    prediction = cv2.resize(prediction, (227, 227))
     prediction_array = np.array([prediction])
-    value = model.predict(prediction_array.reshape(-1, 224, 224, 1))
-    if value[0][0] > 0.98:
-        print("CAR")
-    elif value[0][1] > 0.98:
-        print("HUMAN")
-    else:
-        print("WHAT IS THAT?")
+    value = model.predict(prediction_array.reshape(-1, 227, 227, 1))
+    if value <= 0.5:
+        print("Car")
+    elif value > 0.5:
+        print("Human")
+
     print("FPS {} ".format(((time.time()-last_time)**(-1))))
     last_time = time.time()
     cv2.imshow("Screen", prediction)
